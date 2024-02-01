3cfc4bed2669024b0457ae28250a1c2223c1dc71
Rob Avery <robertaveryiv@gmail.com>
2024-01-31 20:56:46 -0500

    Create boilerplate code for API to get and create users

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,17 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import routes from 'routes';
 
-const MONGO_DB_URL = ""
+const MONGO_DB_URL = "mongodb+srv://ravery90:ravery90@cluster0.vhq5yur.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
 
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_DB_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', routes());
 
 app.use(cors({
     credentials: true
