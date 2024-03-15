2fb27c2e9363d141b0cc2064ce9c9037a45aa098
Rob Avery <robertaveryiv@gmail.com>
2024-03-14 23:47:11 -0400

    Removed requirement to view film

--- src/routes/films.ts
+++ src/routes/films.ts
@@ -4,8 +4,8 @@ import { deleteFilm, getAllFilms, newFilm, searchFilms, updateFilm } from '../co
 import { isAuthenticated } from '../middlewares';
 
 export default (router: express.Router) => {
-    router.get('/film', isAuthenticated, getAllFilms);
-    router.get('/film/:id', isAuthenticated, getAllFilms);
+    router.get('/film', getAllFilms);
+    router.get('/film/:id', getAllFilms);
     router.post('/film', isAuthenticated, newFilm);
     router.put('/film/:id', isAuthenticated, updateFilm);
     router.delete('/film/:id', isAuthenticated, deleteFilm);
